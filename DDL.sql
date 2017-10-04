410a75123eba5e97fd52a7dcb433e2cdce92f236
Jhorkman Bernal <jhorber95@gmail.com>
2017-10-03 22:19:03 -0500

    Datos sql

---
+++ DDL.sql
@@ -0,0 +1,5 @@
+CREATE TABLE public.persona(
+	per_id serial NOT NULL,
+	per_nombre character varying(30),
+	per_apellido character varying(30)
+);
---
+++ INSERT.sql
@@ -0,0 +1,2 @@
+INSERT INTO persona(per_nombre,per_apellido) 
+VALUES ('Jhon', 'Serrano'),('Camilo','Sarmiento');
